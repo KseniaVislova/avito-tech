81d0f5722dde488d19dfd681c165343698fb44c8
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-17 18:37:49 +0300

    feat: add button for updating news

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import {useDispatch} from 'react-redux';
 import NewsList from './components/NewsList';
 import News from './components/News';
-import { ThemeProvider, Container } from 'react-bootstrap';
+import { fetchNews } from './asyncActions/fetchNews';
+import { removeNews} from './actions/updateNews';
+import { ThemeProvider, Container, Button } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
   Route,
-} from "react-router-dom";
+} from "react-router-dom";;
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const updateNews = () => {
+    dispatch(removeNews());
+    dispatch(fetchNews())
+  }
+
   return (
     <ThemeProvider
       breakpoints={['xxl', 'xl', 'lg', 'md', 'sm']}
     >
       <Container>
         <h1 className="text-center">Hacker News</h1>
+        <Button variant="outline-success" onClick={updateNews}>Update news</Button>
         <Router>
           <Route path="/" exact component={NewsList} />
           <Route path="/test" component={News} />
